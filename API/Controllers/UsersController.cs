1a2fd8d9b06a04bba585d6d1563bf38bd7504b7c
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-06-05 20:30:56 +0300

    add UsersController.cs and test GetAllUsers()

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+    }
+}
